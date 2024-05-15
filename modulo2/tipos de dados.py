0111d84e1e4239b82af89b042c9cb6f374a2edba
Martucoweb <164926794+Martucoweb@users.noreply.github.com>
2024-05-14 21:18:24 -0300

    tipos de dados

---
+++ modulo2/tipos de dados.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+v1=5
+v2=2
+print(type(v1))
+print(type(v2))
+
+divisao=v1/v2
+print(divisao)
+print(type(divisao))
+
+
+
+# #questão 2
+# 
+
+# In[4]:
+
+
+juros=1.01
+saldo=500.0
+print(saldo)
+saldo=saldo*juros
+print(saldo)
+saldo=saldo*juros
+print(saldo)
+saldo=saldo*juros
+print(saldo)
+
+print(type(saldo))
+
+
+# In[ ]:
+
+
+
+
