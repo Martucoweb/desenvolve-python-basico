7cc748eb538a09c5aeb094bdfb59729e65aa03ba
Martucoweb <164926794+Martucoweb@users.noreply.github.com>
2024-05-16 22:36:27 -0300

    aula2_questao1.py

---
+++ modulo2/relacionais.py
@@ -0,0 +1,19 @@
+#entrada de dados 
+#idade de Juliana
+# idade de Cris
+idade_juliana = int(input())
+idade_cris = int(input())
+
+# processamento 
+#true se ambos forem maior de idade 
+# <exp1>juliana e maior de idade
+#<exp2>cris e maior de idade
+#<exp1> and<exp2>
+#false em qualquer outro caso
+
+pode_entrar= idade_juliana >= 18 and idade_cris >= 18
+
+# saida
+print (pode_entrar)
+
+
