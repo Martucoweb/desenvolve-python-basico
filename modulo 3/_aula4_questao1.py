ab361c279e2e64471f4897ceee3bcdd6543ed2bd
Martucoweb <164926794+Martucoweb@users.noreply.github.com>
2024-05-27 21:37:48 -0300

    modulo3

---
+++ modulo 3/_aula4_questao1.py
@@ -0,0 +1,74 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "id": "2f915db0-62c5-4009-b530-10b737cd507d",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdin",
+     "output_type": "stream",
+     "text": [
+      " 1\n",
+      " 2\n"
+     ]
+    },
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "e impar\n"
+     ]
+    }
+   ],
+   "source": [
+    "n1, n2 = int(input()) , int(input())\n",
+    "if n1+n2 % 2 == 0:\n",
+    "    print(\"E par\")\n",
+    "else:\n",
+    "    print(\"e impar\")"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "2d6445ec-9258-4ef1-a9a5-ed2b0b706eca",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "n1, n2 = int(input()) , int(input())\n",
+    "print(\"E par\" if (n1+n2) % 2 == 0 else \"E impar\")"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "396b9f20-f90a-4169-830a-d90c7c470f55",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.11.7"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
